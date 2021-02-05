a2396693d8435ba78efe7294638102c819260b89
Nico Felbinger <26925347+felbinger@users.noreply.github.com>
2021-02-05 11:28:24 +0100

    Improved postinstall.sh - fixed a few bugs

--- postinstall.sh
+++ postinstall.sh
@@ -6,6 +6,9 @@ ADM_GID=997
 ADM_HOME='/home/admin'
 ADM_USERS=('user')
 
+# create compose files (disable if you would like to import a backup)
+ADD_COMPOSE=1
+
 declare -A STACKS=(\
   ["main"]="192.168.100.0/24"
 )
@@ -16,9 +19,6 @@ declare -A HELPER=(\
   ["monitoring"]="192.168.2.0/24"
 )
 ### END of CONFIGURATION ###
-
-# print all executed commands
-set -x
 
 function install_docker_compose() {
   curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -38,11 +38,9 @@ function create_compose() {
   echo -e "version: '3.9'\n" >${compose}
 
   # define services
-  echo -e "services:\n" >>${compose}
-  if [[ ${#SERVICES} == 0 ]]; then
-    echo -e "  test:" >>${compose}
-    echo -e "    image: hello-world\n" >>${compose}
-  fi
+  echo -e "services:" >>${compose}
+  echo -e "  test:" >>${compose}
+  echo -e "    image: hello-world" >>${compose}
 
   echo -e "\n" >>${compose}
 
@@ -68,12 +66,23 @@ if [[ $(/usr/bin/id -u) != "0" ]]; then
   exit 1
 fi
 
+if [[ ${RERUN} == 1 ]]; then
+  echo "You already ran the script, if you really want to run the script again set 'RERUN=0'. (This might break your system!)"
+  exit 1
+fi
+
+# add rerun=1 variable to prevent postinstall to be executed multiple times
+sed -i '2 i\RERUN=1' ${0}
+
+# print all executed commands
+set -x
+
 apt-get update
 
 # install docker if not already installed
 if [[ -z $(which docker) ]]; then
   if [[ -z $(which curl) ]]; then
-    apt-get install curl
+    apt-get install -y curl
   fi
   curl https://get.docker.com | bash
 fi
@@ -87,15 +96,20 @@ fi
 [[ "${ADM_HOME}" == */ ]] && ADM_HOME="${ADM_HOME::-1}"
 
 # create admin group, add members to group and set permissions
-groupadd -g ${ADM_GID} ${ADM_NAME}
+/usr/sbin/groupadd -g ${ADM_GID} ${ADM_NAME}
 mkdir ${ADM_HOME}
 chown -R root:${ADM_NAME} ${ADM_HOME}
 chmod -R 775 ${ADM_HOME}
-for user in ${ADM_USERS}; do
-  adduser ${user}
-  adduser ${user} ${ADM_NAME}
-  echo -e '\nalias dc="sudo docker-compose "\n' | tee -a /home/${user}/.bashrc > /dev/null
-  echo -e '\nalias ctop="sudo ctop"\n' | tee -a /home/${user}/.bashrc > /dev/null
+for user in ${ADM_USERS[@]}; do
+  if [ ! $(sed -n "/^${user}/p" /etc/passwd) ]; then
+    /usr/sbin/useradd -m --shell=/bin/bash ${user}
+  fi
+  /usr/sbin/usermod --append --groups=sudo,${ADM_NAME} ${user}
+
+  echo -e '\nalias dc="sudo docker-compose "' | tee -a /home/${user}/.bashrc > /dev/null
+  echo -e 'alias ctop="sudo ctop"\n' | tee -a /home/${user}/.bashrc > /dev/null
+
+  ln -s ${ADM_HOME} /home/${user}/admin
 done
 
 # create helper networks
@@ -115,20 +129,24 @@ for name in ${!STACKS[@]}; do
   docker_network_create ${name} ${subnet}
 
   # create docker-compose.yml
-  compose="${ADM_HOME}/services/${name}/docker-compose.yml"
-  create_compose ${compose}
+  if [ ${ADD_COMPOSE} == 1 ]; then
+    compose="${ADM_HOME}/services/${name}/docker-compose.yml"
+    create_compose ${compose}
+  fi
 done
 
-# install ctop
 if [[ -z $(which wget) ]]; then
   apt-get install -y wget
 fi
-wget https://github.com/bcicen/ctop/releases/download/v0.7.3/ctop-0.7.3-linux-amd64 -O /usr/local/bin/ctop
-chmod +x /usr/local/bin/ctop
 
-# add tools
-if [[ -z $(which wget) ]]; then
-  apt-get install -y wget
+# ctop.sh
+if [ -z $(which /usr/local/bin/ctop) ]; then
+  wget https://github.com/bcicen/ctop/releases/download/v0.7.3/ctop-0.7.3-linux-amd64 -O /usr/local/bin/ctop
+  chmod +x /usr/local/bin/ctop
 fi
-wget https://github.com/felbinger/DNV/releases/download/v0.1/dnv -O /usr/local/bin/dnv
-chmod +x /usr/local/bin/dnv
+
+# docker network viewer
+if [ -z $(which /usr/local/bin/dnv) ]; then
+  wget https://github.com/felbinger/DNV/releases/download/v0.1/dnv -O /usr/local/bin/dnv
+  chmod +x /usr/local/bin/dnv
+fi
